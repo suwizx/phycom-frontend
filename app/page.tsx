a9ada5c0f2488b71c89a3bc4860501e7a0f43710
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-11-01 03:30:07 +0700

    feat: Add layout, page, robots, sitemap" without s

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import { Card } from "@/components/ui/card";
 import Weather from "./_components/Weather";
+import LogTable from "./_components/LogTable";
 
 export default function Home() {
   return (
     <div className="w-full flex justify-center">
-      <div className="container">
-        <div className="flex flex-col lg:flex-row gap-4 p-4">
-          <Card className="aspect-video w-225 max-w-full">
-
-          </Card>
+      <div className="container flex flex-col gap-4 p-4">
+        <div className="flex flex-col lg:flex-row gap-4">
+          <Card className="aspect-video w-225 max-w-full"></Card>
           <Card className="flex-1 flex items-start lg:items-end p-6">
             <Weather />
           </Card>
         </div>
+        <Card className="p-6">
+          <LogTable />
+        </Card>
       </div>
     </div>
   );
