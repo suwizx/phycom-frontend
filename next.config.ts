2b973652be9a8a0983e0d2e7ae5a7f30f692f651
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-11-01 03:30:07 +0700

    feat: or refactor? It's adding new config and depe

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {}, // Empty turbopack config to silence webpack warning
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
